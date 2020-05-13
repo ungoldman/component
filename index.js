668661a818881d9a8b755294f3f8386dcd24dd08
Nate Goldman <ungoldman@gmail.com>
2020-05-13 00:13:32 -0700

    convert to class, remove timing

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const document = require('global/document')
-const nanotiming = require('nanotiming')
 const morph = require('nanomorph')
 const onload = require('on-load')
 const assert = require('assert')
@@ -7,150 +6,188 @@ const assert = require('assert')
 const OL_KEY_ID = onload.KEY_ID
 const OL_ATTR_ID = onload.KEY_ATTR
 
-module.exports = Nanocomponent
-
 function makeID () {
   return 'ncid-' + Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1)
 }
 
-function Nanocomponent (name) {
-  this._hasWindow = typeof window !== 'undefined'
-  this._id = null // represents the id of the root node
-  this._ncID = null // internal nanocomponent id
-  this._olID = null
-  this._proxy = null
-  this._loaded = false // Used to debounce on-load when child-reordering
-  this._rootNodeName = null
-  this._name = name || 'nanocomponent'
-  this._rerender = false
+class Nanocomponent {
+  constructor () {
+    this._hasWindow = typeof window !== 'undefined'
+    this._id = null // represents the id of the root node
+    this._ncID = null // internal nanocomponent id
+    this._olID = null
+    this._proxy = null
+    this._loaded = false // Used to debounce on-load when child-reordering
+    this._rootNodeName = null
+    this._rerender = false
 
-  this._handleLoad = this._handleLoad.bind(this)
-  this._handleUnload = this._handleUnload.bind(this)
+    this._handleLoad = this._handleLoad.bind(this)
+    this._handleUnload = this._handleUnload.bind(this)
 
-  this._arguments = []
+    this._arguments = []
+  }
 
-  const self = this
+  // public methods
 
-  Object.defineProperty(this, 'element', {
-    get: function () {
-      const el = document.getElementById(self._id)
-      if (el) return el.dataset.nanocomponent === self._ncID ? el : undefined
+  createElement () {
+    throw new Error('nanocomponent: createElement should be implemented!')
+  }
+
+  update () {
+    return true
+  }
+
+  render () {
+    const self = this
+    const args = new Array(arguments.length)
+    let el
+
+    for (let i = 0; i < arguments.length; i++) {
+      args[i] = arguments[i]
     }
-  })
-}
 
-Nanocomponent.prototype.render = function () {
-  const renderTiming = nanotiming(this._name + '.render')
-  const self = this
-  const args = new Array(arguments.length)
-  let el
-
-  for (let i = 0; i < arguments.length; i++) args[i] = arguments[i]
-  if (!this._hasWindow) {
-    const createTiming = nanotiming(this._name + '.create')
-    el = this.createElement.apply(this, args)
-    createTiming()
-    renderTiming()
-    return el
-  } else if (this.element) {
-    el = this.element // retain reference, as the ID might change on render
-    const updateTiming = nanotiming(this._name + '.update')
-    const shouldUpdate = this._rerender || this.update.apply(this, args)
-    updateTiming()
-    if (this._rerender) this._rerender = false
-    if (shouldUpdate) {
-      const desiredHtml = this._handleRender(args)
-      const morphTiming = nanotiming(this._name + '.morph')
-      morph(el, desiredHtml)
-      morphTiming()
-      if (this.afterupdate) this.afterupdate(el)
+    if (!this._hasWindow) {
+      el = this.createElement.apply(this, args)
+      return el
     }
-    if (!this._proxy) { this._proxy = this._createProxy() }
-    renderTiming()
-    return this._proxy
-  } else {
+
+    if (this.element) {
+      el = this.element // retain reference, as the ID might change on render
+
+      const shouldUpdate = this._rerender || this.update.apply(this, args)
+
+      if (this._rerender) {
+        this._rerender = false
+      }
+
+      if (shouldUpdate) {
+        const desiredHtml = this._handleRender(args)
+
+        morph(el, desiredHtml)
+
+        if (this.afterupdate) this.afterupdate(el)
+      }
+
+      if (!this._proxy) {
+        this._proxy = this._createProxy()
+      }
+
+      return this._proxy
+    }
+
     this._reset()
+
     el = this._handleRender(args)
+
     if (this.beforerender) this.beforerender(el)
     if (this.load || this.unload || this.afterreorder) {
       onload(el, self._handleLoad, self._handleUnload, self._ncID)
       this._olID = el.dataset[OL_KEY_ID]
     }
-    renderTiming()
+
     return el
   }
-}
 
-Nanocomponent.prototype.rerender = function () {
-  assert(this.element, 'nanocomponent: cant rerender on an unmounted dom node')
-  this._rerender = true
-  this.render.apply(this, this._arguments)
-}
+  rerender () {
+    assert(this.element, 'nanocomponent: cant rerender on an unmounted dom node')
 
-Nanocomponent.prototype._handleRender = function (args) {
-  const createElementTiming = nanotiming(this._name + '.createElement')
-  const el = this.createElement.apply(this, args)
-  createElementTiming()
-  if (!this._rootNodeName) this._rootNodeName = el.nodeName
-  assert(el instanceof window.Element, 'nanocomponent: createElement should return a single DOM node')
-  assert(this._rootNodeName === el.nodeName, 'nanocomponent: root node types cannot differ between re-renders')
-  this._arguments = args
-  return this._brandNode(this._ensureID(el))
-}
-
-Nanocomponent.prototype._createProxy = function () {
-  const proxy = document.createElement(this._rootNodeName)
-  const self = this
-  this._brandNode(proxy)
-  proxy.id = this._id
-  proxy.setAttribute('data-proxy', '')
-  proxy.isSameNode = function (el) {
-    return (el && el.dataset.nanocomponent === self._ncID)
+    this._rerender = true
+    this.render.apply(this, this._arguments)
   }
-  return proxy
-}
 
-Nanocomponent.prototype._reset = function () {
-  this._ncID = makeID()
-  this._olID = null
-  this._id = null
-  this._proxy = null
-  this._rootNodeName = null
-}
+  // getters
 
-Nanocomponent.prototype._brandNode = function (node) {
-  node.setAttribute('data-nanocomponent', this._ncID)
-  if (this._olID) node.setAttribute(OL_ATTR_ID, this._olID)
-  return node
-}
+  get element () {
+    const el = document.getElementById(this._id)
 
-Nanocomponent.prototype._ensureID = function (node) {
-  if (node.id) this._id = node.id
-  else node.id = this._id = this._ncID
-  // Update proxy node ID if it changed
-  if (this._proxy && this._proxy.id !== this._id) this._proxy.id = this._id
-  return node
-}
-
-Nanocomponent.prototype._handleLoad = function (el) {
-  if (this._loaded) {
-    if (this.afterreorder) this.afterreorder(el)
-    return // Debounce child-reorders
+    if (el) {
+      return el.dataset.nanocomponent === this._ncID ? el : undefined
+    }
   }
-  this._loaded = true
-  if (this.load) this.load(el)
+
+  // private methods
+
+  _handleRender (args) {
+    const el = this.createElement.apply(this, args)
+
+    if (!this._rootNodeName) {
+      this._rootNodeName = el.nodeName
+    }
+
+    assert(el instanceof window.Element, 'nanocomponent: createElement should return a single DOM node')
+    assert(this._rootNodeName === el.nodeName, 'nanocomponent: root node types cannot differ between re-renders')
+
+    this._arguments = args
+
+    return this._brandNode(this._ensureID(el))
+  }
+
+  _createProxy () {
+    const proxy = document.createElement(this._rootNodeName)
+    const self = this
+
+    this._brandNode(proxy)
+
+    proxy.id = this._id
+    proxy.setAttribute('data-proxy', '')
+    proxy.isSameNode = function (el) {
+      return el && el.dataset.nanocomponent === self._ncID
+    }
+
+    return proxy
+  }
+
+  _reset () {
+    this._ncID = makeID()
+    this._olID = null
+    this._id = null
+    this._proxy = null
+    this._rootNodeName = null
+  }
+
+  _brandNode (node) {
+    node.setAttribute('data-nanocomponent', this._ncID)
+
+    if (this._olID) {
+      node.setAttribute(OL_ATTR_ID, this._olID)
+    }
+
+    return node
+  }
+
+  _ensureID (node) {
+    if (node.id) {
+      this._id = node.id
+    } else {
+      node.id = this._id = this._ncID
+    }
+
+    // Update proxy node ID if it changed
+    if (this._proxy && this._proxy.id !== this._id) {
+      this._proxy.id = this._id
+    }
+
+    return node
+  }
+
+  _handleLoad (el) {
+    if (this._loaded) {
+      if (this.afterreorder) this.afterreorder(el)
+      return // Debounce child-reorders
+    }
+
+    this._loaded = true
+
+    if (this.load) this.load(el)
+  }
+
+  _handleUnload (el) {
+    if (this.element) return // Debounce child-reorders
+
+    this._loaded = false
+
+    if (this.unload) this.unload(el)
+  }
 }
 
-Nanocomponent.prototype._handleUnload = function (el) {
-  if (this.element) return // Debounce child-reorders
-  this._loaded = false
-  if (this.unload) this.unload(el)
-}
-
-Nanocomponent.prototype.createElement = function () {
-  throw new Error('nanocomponent: createElement should be implemented!')
-}
-
-Nanocomponent.prototype.update = function () {
-  throw new Error('nanocomponent: update should be implemented!')
-}
+module.exports = Nanocomponent
